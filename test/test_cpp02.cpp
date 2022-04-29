5c6dd14d1ab9d709c5cefe96b4421fb594fdf27a
falloutlast <falloutlast888@gmail.com>
2022-04-29 17:25:01 +0800

    a

--- test/test_cpp02.cpp
+++ test/test_cpp02.cpp
@@ -12,7 +12,7 @@ int Data::data=100;//类外定义+初始化
 void test01()
 {
     //data是静态成员变量 是属于类 可以通过类名称::直接访问
-    cout<<Data::data<<endl;//100
+    cout<<Data::data<<endl;//100                                                                                                                                                                                                                                                               
     //赋值
     Data::data = 200;
     cout<<Data::data<<endl;//200
